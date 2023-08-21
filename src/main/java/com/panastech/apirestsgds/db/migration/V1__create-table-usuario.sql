047b240e3a3407fcb53cac6f4fd9927ae15b8d45
PHRGusmão <phrgusmao@gmail.com>
2023-08-21 17:31:14 -0300

    Adesão de comandos MySQL

--- src/main/java/com/panastech/apirestsgds/db/migration/V1__create-table-usuario.sql
+++ src/main/java/com/panastech/apirestsgds/db/migration/V1__create-table-usuario.sql
@@ -1,2 +1,12 @@
-
+create table usuario (
+	id_usuario int unique auto_increment,
+    nome varchar(60) not null,
+    cpf varchar(11)not null unique,
+    cargo enum ('Paciente','Profissional da Saúde','Recepcionista','Administrador','Técnico de Manutenção') not null,
+    email varchar(40) not null,
+    senha varchar(90) not null,
+    telefone varchar(14),
+    primary key (id_usuario)
+	
+) default charset = UTF8MB4;
 
