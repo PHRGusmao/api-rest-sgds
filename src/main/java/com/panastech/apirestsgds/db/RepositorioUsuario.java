c418d192b7333024260563ae80b9d948f9da4f35
Tai.Soares <taisoarescarvalho@gmail.com>
2023-08-24 20:39:39 -0300

    versão-1.0.1

--- src/main/java/com/panastech/apirestsgds/db/RepositorioUsuario.java
+++ src/main/java/com/panastech/apirestsgds/db/RepositorioUsuario.java
@@ -1,9 +1,19 @@
 package com.panastech.apirestsgds.db;
 
+import java.util.Optional;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.panastech.apirestsgds.Entidade.Usuario;
 
-public interface RepositorioUsuario extends JpaRepository<Usuario, Long>{
-    
+public interface RepositorioUsuario extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByCpf(String cpf);
+
+    List<Usuario> findByNomeContaining(String nome);
+
+    List<Usuario> findByEmailContaining(String email);
+
+    List<Usuario> findByTelefoneContaining(String telefone);
+
+    List<Usuario> findByCargoContaining(String cargo);
 }
--- src/main/java/com/panastech/apirestsgds/db/controllers/UsuarioRest.java
+++ src/main/java/com/panastech/apirestsgds/db/controllers/UsuarioRest.java
@@ -1,10 +1,13 @@
 package com.panastech.apirestsgds.db.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,27 +24,100 @@ public class UsuarioRest {
     private RepositorioUsuario repositorioUsuario;
 
     @GetMapping
-    public List<Usuario> listar(){
+    public List<Usuario> listar() {
         return repositorioUsuario.findAll();
     }
 
-    @PostMapping
-    public void salvar(@RequestBody List<Usuario> listUsuario){
-        for(Usuario usuario: listUsuario){
+    @GetMapping("/buscaruserid/{id}")
+    public ResponseEntity<Usuario> getUsuarioById(@PathVariable Long id) {
+        Optional<Usuario> usuario = repositorioUsuario.findById(id);
+        if (usuario.isPresent()) {
+            return ResponseEntity.ok(usuario.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/buscarusercpf/{cpf}")
+    public ResponseEntity<Usuario> getUsuarioByCpf(@PathVariable String cpf) {
+        Optional<Usuario> usuario = repositorioUsuario.findByCpf(cpf);
+        if (usuario.isPresent()) {
+            return ResponseEntity.ok(usuario.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/buscarusernome/{nome}")
+    public ResponseEntity<List<Usuario>> getUsuarioByNome(@PathVariable String nome) {
+        List<Usuario> usuario = repositorioUsuario.findByNomeContaining(nome);
+        if (usuario.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(usuario);
+        }
+    }
+
+    @GetMapping("/buscaruseremail/{email}")
+    public ResponseEntity<List<Usuario>> getUsuarioByEmail(@PathVariable String email) {
+        List<Usuario> usuario = repositorioUsuario.findByEmailContaining(email);
+        if (usuario.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(usuario);
+        }
+    }
+
+    @GetMapping("/buscarusertelefone/{telefone}")
+    public ResponseEntity<List<Usuario>> getUsuarioByTelefone(@PathVariable String telefone) {
+        List<Usuario> usuario = repositorioUsuario.findByTelefoneContaining(telefone);
+        if (usuario.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(usuario);
+        }
+    }
+
+    @GetMapping("/buscarusercargo/{cargo}")
+    public ResponseEntity<List<Usuario>> getUsuarioByCargo(@PathVariable String cargo) {
+        List<Usuario> usuario = repositorioUsuario.findByCargoContaining(cargo);
+        if (usuario.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(usuario);
+        }
+    }
+
+    @PostMapping("/adicionarUsuario")
+    public void salvar(@RequestBody List<Usuario> listUsuario) {
+        for (Usuario usuario : listUsuario) {
             repositorioUsuario.save(usuario);
         }
     }
 
-    @PutMapping
-    public void alterar(@RequestBody List<Usuario> listUsuario){
-        for(Usuario usuario: listUsuario){
-            repositorioUsuario.save(usuario);
+    @PutMapping("/alterarUsuario/{cpf}")
+    public ResponseEntity<Usuario> alterarUsuarioByCpf(@PathVariable String cpf,
+            @RequestBody Usuario usuarioAtualizado) {
+        Optional<Usuario> usuarioExistente = repositorioUsuario.findByCpf(cpf);
+        if (usuarioExistente.isPresent()) {
+            Usuario usuario = usuarioExistente.get();
+            usuario.setNome(usuarioAtualizado.getNome());
+            usuario.setCpf(usuarioAtualizado.getCpf());
+            usuario.setEmail(usuarioAtualizado.getEmail());
+            usuario.setSenha(usuarioAtualizado.getSenha());
+            usuario.setTelefone(usuarioAtualizado.getTelefone());
+            usuario.setCargo(usuarioAtualizado.getCargo());
+
+            usuarioAtualizado = repositorioUsuario.save(usuario);
+            return ResponseEntity.ok(usuarioAtualizado);
+        } else {
+            return ResponseEntity.notFound().build();
         }
     }
 
     @DeleteMapping
-    public void delete(@RequestBody List<Usuario> listUsuario){
-       for(Usuario usuario: listUsuario){
+    public void delete(@RequestBody List<Usuario> listUsuario) {
+        for (Usuario usuario : listUsuario) {
             repositorioUsuario.delete(usuario);
         }
     }
