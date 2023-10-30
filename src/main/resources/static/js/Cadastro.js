7bb03c968b7aa604d18c5bc83c4d01ac0d57318d
Tai.Soares <taisoarescarvalho@gmail.com>
2023-10-30 13:18:17 -0300

    alteração para login

--- src/main/resources/static/js/Cadastro.js
+++ src/main/resources/static/js/Cadastro.js
@@ -1,27 +1,3 @@
-function fazerLogin() {
-    const cpf = document.getElementById('inputCpf').value;
-    const senha = document.getElementById('inputSenha').value;
-
-    fetch(`https://panastech.com/usuarios/search/findByCpf?cpf=${cpf}`)
-        .then(response => response.json())
-        .then(data => {
-            const usuario = data._embedded.usuarios[0];
-
-            if (usuario) {
-                if (usuario.senha === senha) {
-                    alert('Login bem-sucedido!');
-                } else {
-                    alert('Senha incorreta');
-                }
-            } else {
-                alert('CPF incorreto');
-            }
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-        });
-}
-
 function enviarDados() {
     console.log("Script carregado")
     const form = document.getElementById('form-cadastro');
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -24,3 +24,29 @@ function nameValidate(){
         console.log('Nome Validado')
     }
 }
+
+
+function fazerLogin() {
+    const cpf = document.getElementById('inputCpf').value;
+    const senha = document.getElementById('inputSenha').value;
+
+    fetch(`https://panastech.com/usuarios/search/findByCpf?cpf=${cpf}`)
+        .then(response => response.json())
+        .then(data => {
+            const usuario = data._embedded.usuarios[0];
+
+            if (usuario) {
+                if (usuario.senha === senha) {
+                    alert('Login bem-sucedido!');
+                    window.location.href = '../root/pages/Perfil.html';
+                } else {
+                    alert('Senha incorreta');
+                }
+            } else {
+                alert('CPF incorreto');
+            }
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+}
