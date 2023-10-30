c0bdf6e8675ffb1303ddb36660492fb32ed2c430
Tai.Soares <taisoarescarvalho@gmail.com>
2023-10-30 14:32:59 -0300

    alteração no js

--- src/main/resources/static/js/FazerLogin.js
+++ src/main/resources/static/js/FazerLogin.js
@@ -3,24 +3,20 @@ function fazerLogin() {
     const senha = document.getElementById('inputSenha').value;
 
     fetch(`https://panastech.com/usuarios/search/findByCpf?cpf=${cpf}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('CPF incorreto');
-        }
-        return response.json();
-    })
-    .then(data => {
-        const usuario = data._embedded.usuarios[0];
-        const senhaDoUsuario = usuario.senha;
+        .then(response => response.json())
+        .then(data => {
+            const usuario = data;
+            const senhaDoUsuario = usuario.senha;
 
-        if (senhaDoUsuario && senhaDoUsuario === senha) {
-            alert('Login bem-sucedido!');
-            window.location.href = '../root/pages/Perfil.html';
-        } else {
-            alert('Senha incorreta');
-        }
-    })
-    .catch(error => {
-        alert(error.message); // Exibe a mensagem de erro, seja "CPF incorreto" ou outro erro
-    });
+            if (senhaDoUsuario && senhaDoUsuario === senha) {
+                alert('Login bem-sucedido!');
+                window.location.href = '../root/pages/Perfil.html';
+            } else {
+                alert('Senha incorreta');
+            }
+        })
+        .catch(error => {
+            alert('CPF incorreto ou usuário não encontrado');
+            console.error('Erro:', error);
+        });
 }
